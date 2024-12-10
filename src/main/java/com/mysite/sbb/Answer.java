fc59cd84db283ee5e7ecb2d483ea7dc94b200b8f
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2024-12-10 14:42:36 +0900

    add entity

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author shjung
+ * @since 24. 12. 10.
+ */
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    private Question question;
+}
